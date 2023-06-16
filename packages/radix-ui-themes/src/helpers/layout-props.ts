48b598f5bfde2138bc2d301842b736fc10651433
Benoît Grélard <benoit.grelard@gmail.com>
2023-06-16 23:54:45 +0100

    expose layout props values

--- packages/radix-ui-themes/src/helpers/layout-props.ts
+++ packages/radix-ui-themes/src/helpers/layout-props.ts
@@ -2,47 +2,48 @@ import { withBreakpoints } from './breakpoints';
 
 import type { Responsive } from './breakpoints';
 
+const positionValues = ['static', 'relative', 'absolute', 'fixed', 'sticky'] as const;
+type Position = (typeof positionValues)[number];
+
+const positionEdgeValues = ['auto', '0', '50%', '100%'] as const;
+type PositionEdge = (typeof positionEdgeValues)[number];
+
+const widthHeightValues = [
+  'auto',
+  'min-content',
+  'max-content',
+  '100%',
+  '0',
+  '1',
+  '2',
+  '3',
+  '4',
+  '5',
+  '6',
+  '7',
+  '8',
+  '9',
+] as const;
+type Width = (typeof widthHeightValues)[number];
+type Height = (typeof widthHeightValues)[number];
+
+const flexShrinkValues = ['0', '1'] as const;
+type FlexShrink = (typeof flexShrinkValues)[number];
+
+const flexGrowValues = ['0', '1'] as const;
+type FlexGrow = (typeof flexGrowValues)[number];
+
 interface LayoutProps {
-  position?: Responsive<'static' | 'relative' | 'absolute' | 'fixed' | 'sticky'>;
-  inset?: Responsive<'auto' | '0' | '50%' | '100%'>;
-  top?: Responsive<'auto' | '0' | '50%' | '100%'>;
-  bottom?: Responsive<'auto' | '0' | '50%' | '100%'>;
-  left?: Responsive<'auto' | '0' | '50%' | '100%'>;
-  right?: Responsive<'auto' | '0' | '50%' | '100%'>;
-  width?: Responsive<
-    | 'auto'
-    | 'min-content'
-    | 'max-content'
-    | '100%'
-    | '0'
-    | '1'
-    | '2'
-    | '3'
-    | '4'
-    | '5'
-    | '6'
-    | '7'
-    | '8'
-    | '9'
-  >;
-  height?: Responsive<
-    | 'auto'
-    | 'min-content'
-    | 'max-content'
-    | '100%'
-    | '0'
-    | '1'
-    | '2'
-    | '3'
-    | '4'
-    | '5'
-    | '6'
-    | '7'
-    | '8'
-    | '9'
-  >;
-  shrink?: Responsive<'0' | '1'>;
-  grow?: Responsive<'0' | '1'>;
+  position?: Responsive<Position>;
+  inset?: Responsive<PositionEdge>;
+  top?: Responsive<PositionEdge>;
+  bottom?: Responsive<PositionEdge>;
+  left?: Responsive<PositionEdge>;
+  right?: Responsive<PositionEdge>;
+  width?: Responsive<Width>;
+  height?: Responsive<Height>;
+  shrink?: Responsive<FlexShrink>;
+  grow?: Responsive<FlexGrow>;
 }
 
 function extractLayoutProps<T extends LayoutProps>(props: T) {
@@ -67,5 +68,13 @@ function withLayout(props: LayoutProps) {
     .join(' ');
 }
 
-export { extractLayoutProps, withLayout };
-export type { LayoutProps };
+export {
+  positionValues,
+  positionEdgeValues,
+  widthHeightValues,
+  flexShrinkValues,
+  flexGrowValues,
+  extractLayoutProps,
+  withLayout,
+};
+export type { Position, PositionEdge, Width, Height, FlexShrink, FlexGrow, LayoutProps };
