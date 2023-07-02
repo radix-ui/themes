1041bb87f5af906d2d6c8b15fdf271f2af77b9d4
Benoît Grélard <benoit.grelard@gmail.com>
2023-07-02 23:21:03 +0100

    Heading: add `align` support

--- packages/radix-ui-themes/src/components/heading.props.ts
+++ packages/radix-ui-themes/src/components/heading.props.ts
@@ -4,6 +4,10 @@ const headingSizes = ['1', '2', '3', '4', '5', '6', '7', '8', '9'] as const;
 type HeadingSize = (typeof headingSizes)[number];
 const headingSizeDefault: HeadingSize = '6';
 
+const headingAlignValues = ['left', 'center', 'right'] as const;
+type HeadingAlign = (typeof headingAlignValues)[number];
+const headingAlignDefault: HeadingAlign | undefined = undefined;
+
 const headingTrimValues = ['normal', 'start', 'end', 'both'] as const;
 type HeadingTrim = (typeof headingTrimValues)[number];
 const headingTrimDefault: HeadingTrim | undefined = undefined;
@@ -13,8 +17,10 @@ const headingColorDefault: ThemeAccentScale | undefined = undefined;
 export {
   headingSizes,
   headingSizeDefault,
+  headingAlignValues,
+  headingAlignDefault,
   headingTrimValues,
   headingTrimDefault,
   headingColorDefault,
 };
-export type { HeadingSize, HeadingTrim };
+export type { HeadingSize, HeadingAlign, HeadingTrim };
--- packages/radix-ui-themes/src/components/heading.tsx
+++ packages/radix-ui-themes/src/components/heading.tsx
@@ -1,10 +1,15 @@
 import * as React from 'react';
 import classNames from 'classnames';
 import { Slot } from '@radix-ui/react-slot';
-import { headingSizeDefault, headingTrimDefault, headingColorDefault } from './heading.props';
+import {
+  headingSizeDefault,
+  headingAlignDefault,
+  headingTrimDefault,
+  headingColorDefault,
+} from './heading.props';
 import { extractMarginProps, withMarginProps, withBreakpoints } from '../helpers';
 
-import type { HeadingSize, HeadingTrim } from './heading.props';
+import type { HeadingSize, HeadingAlign, HeadingTrim } from './heading.props';
 import type { PropsWithoutRefOrColor, MarginProps, Responsive } from '../helpers';
 import type { ThemeAccentScale } from '../theme';
 
@@ -12,6 +17,7 @@ type HeadingElement = React.ElementRef<'h1'>;
 interface HeadingProps extends PropsWithoutRefOrColor<'h1'>, MarginProps {
   asChild?: boolean;
   size?: Responsive<HeadingSize>;
+  align?: Responsive<HeadingAlign>;
   trim?: Responsive<HeadingTrim>;
   color?: ThemeAccentScale | 'color';
 }
@@ -21,6 +27,7 @@ const Heading = React.forwardRef<HeadingElement, HeadingProps>((props, forwarded
     className,
     asChild = false,
     size = headingSizeDefault,
+    align = headingAlignDefault,
     trim = headingTrimDefault,
     color = headingColorDefault,
     ...headingProps
@@ -35,6 +42,7 @@ const Heading = React.forwardRef<HeadingElement, HeadingProps>((props, forwarded
         'rui-Heading',
         withBreakpoints(size, 'size'),
         withMarginProps(marginProps),
+        withBreakpoints(align, 'rui-ta'),
         withBreakpoints(trim, 'rui-lt'),
         className
       )}
