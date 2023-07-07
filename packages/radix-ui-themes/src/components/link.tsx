d819faecf74c3d4b76643020df77fbc7af0f2632
Benoît Grélard <benoit.grelard@gmail.com>
2023-07-07 02:46:20 +0100

    Update link.tsx

--- packages/radix-ui-themes/src/components/link.tsx
+++ packages/radix-ui-themes/src/components/link.tsx
@@ -11,27 +11,12 @@ interface LinkProps extends PropsWithoutRefOrColor<'a'>, MarginProps, LinkOwnPro
   asChild?: boolean;
 }
 const Link = React.forwardRef<LinkElement, LinkProps>((props, forwardedRef) => {
-  const {
-    children,
-    className,
-    asChild = false,
-    size = linkPropDefs.size.default,
-    weight = linkPropDefs.weight.default,
-    trim = linkPropDefs.trim.default,
-    color = linkPropDefs.color.default,
-    highContrast = linkPropDefs.highContrast.default,
-    ...linkProps
-  } = props;
+  const { children, className, asChild = false, ...linkProps } = props;
   return (
     <Text
       {...linkProps}
       ref={forwardedRef}
       asChild
-      size={size}
-      weight={weight}
-      trim={trim}
-      color={color}
-      highContrast={highContrast}
       className={classNames(className, 'rui-reset-a', 'rui-reset-button', 'rui-Link')}
     >
       {asChild ? children : <a>{children}</a>}
