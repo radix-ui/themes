641032495a35c7a69c70328172f7de362f3b1bcf
Vlad Moroz <hi@vladmoroz.com>
2023-08-06 16:54:12 +0200

    Add Scroll Area to explore components

--- apps/playground/app/explore-components/page.tsx
+++ apps/playground/app/explore-components/page.tsx
@@ -3483,6 +3483,56 @@ export default function ExploreComponents() {
                   Univers bold condensed more legible rather than less
                 </Box>
 
+                <Heading mb="5">Scroll Area</Heading>
+                <Flex align="start" direction="column" my="6">
+                  <Card size="2">
+                    <Inset>
+                      <ScrollArea
+                        type="always"
+                        scrollbars="vertical"
+                        style={{
+                          width: 520,
+                          height: 180,
+                          '--scrollarea-scrollbar-margin-top': 'var(--space-3)',
+                          '--scrollarea-scrollbar-margin-bottom': 'var(--space-3)',
+                          '--scrollarea-scrollbar-margin-left': 'var(--space-3)',
+                          '--scrollarea-scrollbar-margin-right': 'var(--space-3)',
+                        }}
+                      >
+                        <Inset p="current" style={{ margin: 0 }}>
+                          <Box pr="5">
+                            <Flex direction="column" gap="4">
+                              <Text as="p" size="2">
+                                Three fundamental aspects of typography are legibility, readability,
+                                and aesthetics. Although in a non-technical sense “legible” and
+                                “readable” are often used synonymously, typographically they are
+                                separate but related concepts.
+                              </Text>
+
+                              <Text as="p" size="2">
+                                Legibility describes how easily individual characters can be
+                                distinguished from one another. It is described by Walter Tracy as
+                                “the quality of being decipherable and recognisable”. For instance,
+                                if a “b” and an “h”, or a “3” and an “8”, are difficult to
+                                distinguish at small sizes, this is a problem of legibility.
+                              </Text>
+
+                              <Text as="p" size="2">
+                                Typographers are concerned with legibility insofar as it is their
+                                job to select the correct font to use. Brush Script is an example of
+                                a font containing many characters that might be difficult to
+                                distinguish. The selection of cases influences the legibility of
+                                typography because using only uppercase letters (all-caps) reduces
+                                legibility.
+                              </Text>
+                            </Flex>
+                          </Box>
+                        </Inset>
+                      </ScrollArea>
+                    </Inset>
+                  </Card>
+                </Flex>
+
                 <Heading mb="5">Separator</Heading>
                 <Flex my="6" style={{ whiteSpace: 'nowrap' }}>
                   <Text size="2">
