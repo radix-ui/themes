350a773a4b4d5308566c201ce0c046f058ab552e
Benoît Grélard <benoit.grelard@gmail.com>
2023-12-19 17:55:29 +0100

    Update skeleton.tsx

--- packages/radix-ui-themes/src/components/skeleton.tsx
+++ packages/radix-ui-themes/src/components/skeleton.tsx
@@ -21,7 +21,7 @@ const Skeleton = React.forwardRef<SkeletonElement, SkeletonProps>((props, forwar
     ...skeletonProps
   } = marginRest;
 
-  if (!loading) return children;
+  if (!loading) return <>{children}</>;
 
   const Tag = React.isValidElement(children) ? Slot : 'span';
 
