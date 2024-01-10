4c3cd018c556fab6510197b49c47f59a3170c25a
Kendall Swarthout <kendallstrautman@gmail.com>
2024-01-10 15:04:01 -0800

    add way more examples

--- apps/playground/app/test-data-list/page.tsx
+++ apps/playground/app/test-data-list/page.tsx
@@ -10,15 +10,18 @@ import {
   Theme,
   Container,
   Text,
+  Heading,
+  Button,
+  IconButton,
 } from '@radix-ui/themes';
-import { StarIcon, MagicWandIcon, StarFilledIcon } from '@radix-ui/react-icons';
+import { StarIcon, MagicWandIcon, StarFilledIcon, InfoCircledIcon } from '@radix-ui/react-icons';
 
 export default function DataListPage() {
   return (
     <html>
       <body>
         <Theme>
-          <Box ml="5">
+          <Box ml="5" style={{ maxWidth: '688px;', margin: 'auto' }}>
             <Flex gap="2" mt="4" direction="column">
               <Text weight="medium" mb="2">
                 Default
@@ -99,6 +102,140 @@ export default function DataListPage() {
                 </DataListItem>
               </DataListRoot>
             </Flex>
+            <Box mb="6">
+              <Heading mb="5" size="3">
+                With varied content
+              </Heading>
+              {/* <DataListRoot layout={{ initial: 'vertical', sm: 'horizontal' }}> */}
+              <DataListRoot direction="row">
+                <DataListItem>
+                  <DataListLabel>Status</DataListLabel>
+                  <DataListData>
+                    <Badge color="green" size="1" style={{ marginLeft: -2 }}>
+                      Active
+                    </Badge>
+                  </DataListData>
+                </DataListItem>
+
+                <DataListItem align="center">
+                  <DataListLabel>Name</DataListLabel>
+                  <DataListData>
+                    <Button size="1">Add</Button>
+                  </DataListData>
+                </DataListItem>
+
+                <DataListItem>
+                  <DataListLabel>Email</DataListLabel>
+                  <DataListData>vlad@workos.com</DataListData>
+                </DataListItem>
+
+                <DataListItem>
+                  <DataListLabel>Organization</DataListLabel>
+                  <DataListData>
+                    <Link href="https://workos.com">WorkOS</Link>
+                  </DataListData>
+                </DataListItem>
+
+                <DataListItem>
+                  <DataListLabel>Long value</DataListLabel>
+                  <DataListData>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla ac nisl et libero
+                    ultricies viverra quis vitae quam. Proin a feugiat metus.
+                  </DataListData>
+                </DataListItem>
+
+                <DataListItem align="center">
+                  <DataListLabel>Authentication methods</DataListLabel>
+                  <DataListData>
+                    <Flex gap="2">
+                      <StarFilledIcon />
+                      <StarFilledIcon />
+                    </Flex>
+                  </DataListData>
+                </DataListItem>
+
+                <DataListItem>
+                  <DataListLabel>Long value</DataListLabel>
+                  <DataListData>
+                    Sed luctus, est id feugiat blandit, sapien nisl lobortis arcu, eu malesuada
+                    nulla ex ut lorem. In odio nisl, consectetur id commodo vel, posuere eu risus.
+                  </DataListData>
+                </DataListItem>
+              </DataListRoot>
+            </Box>
+
+            <Box mb="6">
+              <Heading mb="5" size="3">
+                With nested flex
+              </Heading>
+              <DataListRoot>
+                <DataListItem>
+                  <DataListLabel width={80}>Appearance</DataListLabel>
+                  <DataListData>
+                    <Flex align="center" gap="1">
+                      <IconButton size="1">
+                        <InfoCircledIcon />
+                      </IconButton>
+                      <Text>System</Text>
+                    </Flex>
+                  </DataListData>
+                </DataListItem>
+              </DataListRoot>
+            </Box>
+
+            <Box mb="6">
+              <Heading mb="5" size="3">
+                One after another
+              </Heading>
+              <DataListRoot>
+                <DataListItem>
+                  <DataListLabel width={130}>Appearance</DataListLabel>
+                  <DataListData>System</DataListData>
+                </DataListItem>
+                <DataListItem>
+                  <DataListLabel width={130}>Radius</DataListLabel>
+                  <DataListData>Medium</DataListData>
+                </DataListItem>
+              </DataListRoot>
+
+              <DataListRoot>
+                <DataListItem>
+                  <DataListLabel width={130}>Page background</DataListLabel>
+                  <DataListData>White</DataListData>
+                </DataListItem>
+                <DataListItem>
+                  <DataListLabel width={130}>Link color</DataListLabel>
+                  <DataListData>Blue</DataListData>
+                </DataListItem>
+              </DataListRoot>
+            </Box>
+
+            <Box mb="6">
+              <Heading mb="5" size="3">
+                With long label
+              </Heading>
+              {/* <DataListRoot layout={{ initial: 'vertical', sm: 'horizontal' }}> */}
+              <DataListRoot direction="row">
+                <DataListItem>
+                  <DataListLabel>Name</DataListLabel>
+                  <DataListData>Vlad Moroz</DataListData>
+                </DataListItem>
+
+                <DataListItem>
+                  <DataListLabel>Email</DataListLabel>
+                  <DataListData>vlad@workos.com</DataListData>
+                </DataListItem>
+
+                <DataListItem>
+                  <DataListLabel>
+                    Lorem ipsum dolor sit amet consectetur adipscing elit
+                  </DataListLabel>
+                  <DataListData>
+                    <Link href="https://workos.com">WorkOS</Link>
+                  </DataListData>
+                </DataListItem>
+              </DataListRoot>
+            </Box>
           </Box>
         </Theme>
       </body>
