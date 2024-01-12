7162ae5606519d1afbf2169872f17dcd0bb6c948
Kendall Swarthout <kendallstrautman@gmail.com>
2024-01-11 16:21:48 -0800

    fix type

--- packages/radix-ui-themes/src/components/data-list.props.ts
+++ packages/radix-ui-themes/src/components/data-list.props.ts
@@ -1,13 +1,11 @@
 import { PropDef, trimProp, textSize } from '../helpers';
 import { textPropDefs } from './text.props';
 
-const directionValues = ['row', 'column'] as const;
 const gapValues = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] as const;
 
 // Todo figure out if we can enum with the template literal..
 export const dataListPropDefs = {
   columns: { type: 'string', default: '200px 1fr', responsive: true },
-  direction: { type: 'enum', values: directionValues, default: undefined, responsive: true },
   gap: { type: 'enum', values: gapValues, default: '4', responsive: true },
   gapX: { type: 'enum', values: gapValues, default: undefined, responsive: true },
   gapY: { type: 'enum', values: gapValues, default: undefined, responsive: true },
@@ -15,7 +13,6 @@ export const dataListPropDefs = {
   trim: trimProp,
 } satisfies {
   columns?: PropDef<`none` | `${string} 1fr`>;
-  direction?: PropDef<(typeof directionValues)[number]>;
   gap?: PropDef<(typeof gapValues)[number]>;
   gapX?: PropDef<(typeof gapValues)[number]>;
   gapY?: PropDef<(typeof gapValues)[number]>;
