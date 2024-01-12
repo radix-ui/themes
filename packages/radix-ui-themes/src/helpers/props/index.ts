b9a14c17c5adb7a2e1c0f453fba228a91d5d9e32
Kendall Swarthout <kendallstrautman@gmail.com>
2024-01-12 15:39:36 -0800

    adds text size prop

--- packages/radix-ui-themes/src/helpers/props/index.ts
+++ packages/radix-ui-themes/src/helpers/props/index.ts
@@ -7,4 +7,5 @@ export * from './margin.props';
 export * from './prop-def';
 export * from './radius.prop';
 export * from './text-align.prop';
+export * from './text-size.prop';
 export * from './weight.prop';
