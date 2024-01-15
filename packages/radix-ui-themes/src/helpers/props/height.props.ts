6907bae0817129f88b5c4d14dfd888342528166c
Vlad Moroz <hi@vladmoroz.com>
2024-01-15 16:13:22 +0100

    Remove unused code

--- packages/radix-ui-themes/src/helpers/props/height.props.ts
+++ packages/radix-ui-themes/src/helpers/props/height.props.ts
@@ -1,6 +1,3 @@
-import classNames from 'classnames';
-import { getResponsiveStyles } from '../responsive';
-import { mergeStyles } from '../merge-styles';
 import { GetPropDefTypes, PropDef } from './prop-def';
 
 const heightValues = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] as const;
@@ -38,48 +35,5 @@ const heightPropDefs = {
 
 type HeightProps = GetPropDefTypes<typeof heightPropDefs>;
 
-function extractHeightProps<T extends HeightProps>(props: T) {
-  const {
-    height = heightPropDefs.height.default,
-    minHeight = heightPropDefs.minHeight.default,
-    maxHeight = heightPropDefs.maxHeight.default,
-    ...rest
-  } = props;
-  return {
-    height,
-    minHeight,
-    maxHeight,
-    rest,
-  };
-}
-
-function getHeightStyles(props: HeightProps) {
-  const [heightClassNames, heightCustomProperties] = getResponsiveStyles({
-    className: 'rt-r-h',
-    customProperty: '--height',
-    propValues: heightPropDefs.height.values,
-    value: props.height,
-  });
-
-  const [minHeightClassNames, minHeightCustomProperties] = getResponsiveStyles({
-    className: 'rt-r-min-h',
-    customProperty: '--min-height',
-    propValues: heightPropDefs.minHeight.values,
-    value: props.minHeight,
-  });
-
-  const [maxHeightClassNames, maxHeightCustomProperties] = getResponsiveStyles({
-    className: 'rt-r-max-h',
-    customProperty: '--max-height',
-    propValues: heightPropDefs.maxHeight.values,
-    value: props.maxHeight,
-  });
-
-  return [
-    classNames(heightClassNames, minHeightClassNames, maxHeightClassNames),
-    mergeStyles(heightCustomProperties, minHeightCustomProperties, maxHeightCustomProperties),
-  ] as const;
-}
-
-export { heightPropDefs, extractHeightProps, getHeightStyles };
+export { heightPropDefs };
 export type { HeightProps };
--- packages/radix-ui-themes/src/helpers/props/width.props.ts
+++ packages/radix-ui-themes/src/helpers/props/width.props.ts
@@ -1,6 +1,3 @@
-import classNames from 'classnames';
-import { getResponsiveStyles } from '../responsive';
-import { mergeStyles } from '../merge-styles';
 import { GetPropDefTypes, PropDef } from './prop-def';
 
 const widthValues = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'] as const;
@@ -38,48 +35,5 @@ const widthPropDefs = {
 
 type WidthProps = GetPropDefTypes<typeof widthPropDefs>;
 
-function extractWidthProps<T extends WidthProps>(props: T) {
-  const {
-    width = widthPropDefs.width.default,
-    minWidth = widthPropDefs.minWidth.default,
-    maxWidth = widthPropDefs.maxWidth.default,
-    ...rest
-  } = props;
-  return {
-    width,
-    minWidth,
-    maxWidth,
-    rest,
-  };
-}
-
-function getWidthStyles(props: WidthProps) {
-  const [widthClassNames, widthCustomProperties] = getResponsiveStyles({
-    className: 'rt-r-w',
-    customProperty: '--width',
-    propValues: widthPropDefs.width.values,
-    value: props.width,
-  });
-
-  const [minWidthClassNames, minWidthCustomProperties] = getResponsiveStyles({
-    className: 'rt-r-min-w',
-    customProperty: '--min-width',
-    propValues: widthPropDefs.minWidth.values,
-    value: props.minWidth,
-  });
-
-  const [maxWidthClassNames, maxWidthCustomProperties] = getResponsiveStyles({
-    className: 'rt-r-max-w',
-    customProperty: '--max-width',
-    propValues: widthPropDefs.maxWidth.values,
-    value: props.maxWidth,
-  });
-
-  return [
-    classNames(widthClassNames, minWidthClassNames, maxWidthClassNames),
-    mergeStyles(widthCustomProperties, minWidthCustomProperties, maxWidthCustomProperties),
-  ] as const;
-}
-
-export { widthPropDefs, extractWidthProps, getWidthStyles };
+export { widthPropDefs };
 export type { WidthProps };
