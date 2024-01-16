43602e4e5fd7265364b901fdc07148584dfb4a5b
Vlad Moroz <hi@vladmoroz.com>
2024-01-16 17:46:30 +0100

    Lint styles (#236)

--- packages/radix-ui-themes/.stylelintrc.js
+++ packages/radix-ui-themes/.stylelintrc.js
@@ -6,7 +6,7 @@ module.exports = {
     // This is so that Tailwind classes work as expected.
     'selector-max-specificity': ['0,1,1'],
     // Enforce prefixes on classnames and keyframes
-    'selector-class-pattern': /^-?rt-|^radix-themes$|^(light|dark)(-theme)?$/,
+    'selector-class-pattern': /^((xs|sm|md|lg|xl):)?-?rt-|^radix-themes$|^(light|dark)(-theme)?$/,
     'keyframes-name-pattern': /^rt-([a-z]|-)+$/,
   },
 };
