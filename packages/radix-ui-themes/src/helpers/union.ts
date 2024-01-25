42b4f07e60d36053514c4c814c009632c621a6e9
Vlad Moroz <hi@vladmoroz.com>
2024-01-25 18:47:45 +0100

    Typo

--- packages/radix-ui-themes/src/helpers/union.ts
+++ packages/radix-ui-themes/src/helpers/union.ts
@@ -1,3 +1,3 @@
-// Creates a union type of string literals with strings, but retains intellisense for the the literals.
+// Creates a union type of string literals with strings, but retains intellisense for the literals.
 // Union<string, 'foo' | 'bar'> => string | 'foo' | 'bar'
 export type Union<S, T extends string | number> = T | Omit<S, T>;
