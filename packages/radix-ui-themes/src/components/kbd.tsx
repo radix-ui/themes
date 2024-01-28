811c441e0ca0bc85c0b292715d01f0c48cbfaa7a
Vlad Moroz <hi@vladmoroz.com>
2024-01-28 16:14:07 +0100

    Add button Kbd styles

--- packages/radix-ui-themes/src/components/kbd.tsx
+++ packages/radix-ui-themes/src/components/kbd.tsx
@@ -4,14 +4,20 @@ import { kbdPropDefs } from './kbd.props';
 import { extractProps, GetPropDefTypes, marginPropDefs } from '../helpers';
 
 import type { MarginProps } from '../helpers';
+import { Slot } from '@radix-ui/react-slot';
 
 type KbdElement = React.ElementRef<'kbd'>;
 type KbdOwnProps = GetPropDefTypes<typeof kbdPropDefs>;
 interface KbdProps extends React.ComponentPropsWithoutRef<'kbd'>, MarginProps, KbdOwnProps {}
-const Kbd = React.forwardRef<KbdElement, KbdProps>((props, forwardedRef) => {
-  const { className, ...kbdProps } = extractProps(props, kbdPropDefs, marginPropDefs);
-  return <kbd {...kbdProps} ref={forwardedRef} className={classNames('rt-Kbd', className)} />;
-});
+const Kbd = React.forwardRef<KbdElement, KbdProps>(
+  ({ asChild = false, ...props }, forwardedRef) => {
+    const Comp = asChild ? Slot : 'kbd';
+    const { className, ...kbdProps } = extractProps(props, kbdPropDefs, marginPropDefs);
+    return (
+      <Comp {...kbdProps} ref={forwardedRef} className={classNames('rt-reset rt-Kbd', className)} />
+    );
+  }
+);
 Kbd.displayName = 'Kbd';
 
 export { Kbd };
