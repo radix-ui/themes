3ca7d7d4dbb0f27659cfb3fad43faaff6fd1b4d0
Vlad Moroz <hi@vladmoroz.com>
2024-02-01 15:09:33 +0100

    Add asChild to button props

--- packages/radix-ui-themes/src/components/base-button.props.ts
+++ packages/radix-ui-themes/src/components/base-button.props.ts
@@ -1,10 +1,11 @@
-import { colorProp, highContrastProp, radiusProp } from '../helpers';
+import { asChildProp, colorProp, highContrastProp, radiusProp } from '../helpers';
 import type { PropDef } from '../helpers';
 
 const sizes = ['1', '2', '3', '4'] as const;
 const variants = ['classic', 'solid', 'soft', 'surface', 'outline', 'ghost'] as const;
 
 const baseButtonPropDefs = {
+  asChild: asChildProp,
   size: { type: 'enum', className: 'rt-r-size', values: sizes, default: '2', responsive: true },
   variant: { type: 'enum', className: 'rt-variant', values: variants, default: 'solid' },
   color: colorProp,
@@ -12,6 +13,7 @@ const baseButtonPropDefs = {
   radius: radiusProp,
   loading: { type: 'boolean', className: 'rt-loading', default: false },
 } satisfies {
+  asChild: typeof asChildProp;
   size: PropDef<(typeof sizes)[number]>;
   variant: PropDef<(typeof variants)[number]>;
   color: typeof colorProp;
