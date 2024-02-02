ddc3891b6d4d8c117e7cdb9bb6c070d9597514d4
Vlad Moroz <hi@vladmoroz.com>
2024-02-02 17:39:50 +0100

    Update data-list.parts.ts

--- packages/radix-ui-themes/src/components/data-list.parts.ts
+++ packages/radix-ui-themes/src/components/data-list.parts.ts
@@ -1,4 +1,4 @@
 export { DataListRoot as Root } from './data-list';
 export { DataListItem as Item } from './data-list';
 export { DataListLabel as Label } from './data-list';
-export { DataListData as Data } from './data-list';
+export { DataListValue as Value } from './data-list';
