0c791680e34f8b6d55c85bcb268907e8de0dc3bf
Benoît Grélard <benoit.grelard@gmail.com>
2024-02-21 10:13:30 +0000

    Update card.tsx

--- packages/radix-ui-themes/src/components/card.tsx
+++ packages/radix-ui-themes/src/components/card.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import classNames from 'classnames';
 import { Slot } from '@radix-ui/react-slot';
 import { cardPropDefs } from './card.props.js';
-import { extractProps, getRoot } from '../helpers/index.js';
+import { extractProps } from '../helpers/index.js';
 import { marginPropDefs } from '../props/index.js';
 
 import type { ComponentPropsWithoutColor } from '../helpers/index.js';
