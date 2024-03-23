e6cc3275087c78c511763bb202a53dccf5f6e617
Benoît Grélard <benoit.grelard@gmail.com>
2024-03-23 10:35:57 +0000

    Em, Quote, Strong: Fix missing style props extraction (#403)
    * Em, Quote, Strong: Fix missing style props extraction
    
    * Update page.tsx

--- packages/radix-ui-themes/src/components/em.tsx
+++ packages/radix-ui-themes/src/components/em.tsx
@@ -1,20 +1,20 @@
 import * as React from 'react';
 import classNames from 'classnames';
 import { Slot } from '@radix-ui/react-slot';
+import { emPropDefs } from './em.props.js';
+import { extractProps } from '../helpers/index.js';
 
 import type { ComponentPropsWithout, RemovedProps } from '../helpers/index.js';
 import type { GetPropDefTypes } from '../props/index.js';
-import { emPropDefs } from './em.props.js';
 
 type EmElement = React.ElementRef<'em'>;
 type EmOwnProps = GetPropDefTypes<typeof emPropDefs>;
 interface EmProps extends ComponentPropsWithout<'em', RemovedProps>, EmOwnProps {}
-const Em = React.forwardRef<EmElement, EmProps>(
-  ({ asChild, className, ...props }, forwardedRef) => {
-    const Comp = asChild ? Slot : 'em';
-    return <Comp {...props} ref={forwardedRef} className={classNames('rt-Em', className)} />;
-  }
-);
+const Em = React.forwardRef<EmElement, EmProps>((props, forwardedRef) => {
+  const { asChild, className, ...emProps } = extractProps(props, emPropDefs);
+  const Comp = asChild ? Slot : 'em';
+  return <Comp {...emProps} ref={forwardedRef} className={classNames('rt-Em', className)} />;
+});
 Em.displayName = 'Em';
 
 export { Em };
